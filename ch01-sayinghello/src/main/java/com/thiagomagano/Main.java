266abcd4804ff4345b32a53aa311f1289377b5fd
Thiago Magano <thiagomagano1993@outlook.com>
2024-02-28 17:14:24 -0300

    V1 of challenge 01 done

---
+++ ch01-sayinghello/src/main/java/com/thiagomagano/Main.java
@@ -0,0 +1,21 @@
+package com.thiagomagano;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n\n*******************************");
+        System.out.println("Challenge 01 - Saiyng Hello");
+        System.out.println("*******************************\n\n");
+
+        System.out.println("Whats is your name? ");
+        String name = sc.nextLine();
+
+        System.out.printf("Hello, %s, nice to meet you \n", name);
+
+        sc.close();
+    }
+}
