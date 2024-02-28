e5d6ff8392821dcdc15714404cb6927817062b71
Thiago Magano <thiagomagano1993@outlook.com>
2024-02-28 16:42:24 -0300

    V1 of ch00-tipcalculator done

--- ch00-tipcalculator/src/main/java/com/githhub/thiagomagano/App.java
+++ ch00-tipcalculator/src/main/java/com/githhub/thiagomagano/App.java
@@ -1,42 +1,42 @@
 package com.githhub.thiagomagano;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Tip Calculator!");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tip Calculator!");
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Quanto deu a conta, meu Chapa? ");
-        String inputAmount = sc.nextLine();
-        System.out.print("Qual é a porcentagem da gorjeta? ");
-        String inputTipRate = sc.nextLine();
- 
-     
+        double billAmount = 0;
+        int tipRate = 0;
 
-        
+        try {
+            System.out.print("Quanto deu a conta, meu Chapa? ");
+            String inputAmount = sc.nextLine();
 
-        double billAmount = Double.parseDouble(inputAmount);
-        int tipRate = Integer.parseInt(inputTipRate);
-        
+            billAmount = Double.parseDouble(inputAmount);
 
-        
+            System.out.print("Qual é a porcentagem da gorjeta? ");
+            String inputTipRate = sc.nextLine();
+
+            tipRate = Integer.parseInt(inputTipRate);
+
+        } catch (Exception e) {
+            System.out.println("Error! Bill Amount Must be a valid value");
+            System.exit(0);
+        }
+
         TipCalculator tc = new TipCalculator(billAmount, tipRate);
-        
 
         System.out.println("O valor da Gorjeta é de: $" + tc.tipValue);
         System.out.println("O valor total da conta é : $" + tc.billTotal);
 
         sc.close();
-       
+
     }
 }
--- ch00-tipcalculator/src/main/java/com/githhub/thiagomagano/TipCalculator.java
+++ ch00-tipcalculator/src/main/java/com/githhub/thiagomagano/TipCalculator.java
@@ -1,42 +1,32 @@
 package com.githhub.thiagomagano;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class TipCalculator {
-        double billAmount;
-        
-        int tipRate;
-        
-        double tipValue;
-        
-        double billTotal;
+    double billAmount;
 
-        public double calculateTip(double billAmount, int tipRate){
-            double tipRatePercentage = tipRate / 100.00;
-            return toNextCent(billAmount * tipRatePercentage);
-        }
+    int tipRate;
 
-        public double calculateTotalBill(double billAmount, double tipValue){
-            return billAmount + tipValue;
-        }
+    double tipValue;
 
-        public double toNextCent(double value) {
-            return Math.round(value * 100) / 100.0;
-        }
+    double billTotal;
 
-        public BigDecimal toBigDecimal(double value) {
-             BigDecimal bd = BigDecimal.valueOf(value);
-             bd = bd.setScale(2, RoundingMode.HALF_UP);
+    public double calculateTip(double billAmount, int tipRate) {
+        double tipRatePercentage = tipRate / 100.00;
+        return toNextCent(billAmount * tipRatePercentage);
+    }
 
-             return bd;
-        }
-        
-        public TipCalculator(double billAmount, int tipRate) {
-            this.billAmount = billAmount;
-            this.tipRate = tipRate;
-            this.tipValue = calculateTip(billAmount, tipRate);
-            this.billTotal = calculateTotalBill(billAmount, this.tipValue);
-        }
-            
+    public double calculateTotalBill(double billAmount, double tipValue) {
+        return billAmount + tipValue;
+    }
+
+    public double toNextCent(double value) {
+        return Math.round(value * 100) / 100.0;
+    }
+
+    public TipCalculator(double billAmount, int tipRate) {
+        this.billAmount = billAmount;
+        this.tipRate = tipRate;
+        this.tipValue = calculateTip(billAmount, tipRate);
+        this.billTotal = calculateTotalBill(billAmount, this.tipValue);
+    }
+
 }
