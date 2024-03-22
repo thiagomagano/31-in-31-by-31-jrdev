7eae6e9ff394d0e6d93ad95200f2ab5928f86a56
Thiago Magano <thiagomagano1993@outlook.com>
2024-03-22 13:34:39 -0300

    CH12 - Tested object with state. Calculate and Print

--- ch12-simple-interest/Investiment.ts
+++ ch12-simple-interest/Investiment.ts
@@ -1,5 +1,27 @@
 export default class Investiment {
+  readonly amount: number;
+  readonly rate: number;
+  readonly time: number;
+
+  readonly worth: number;
+
+  constructor(amount: number, rate: number, time: number) {
+    this.amount = amount;
+    this.rate = rate;
+    this.time = time;
+
+    this.worth = this.calculate();
+  }
+
+  public calculate(): number {
+    return this.amount * (1 + (this.rate / 100) * this.time);
+  }
+
   public static calculate(amount: number, rate: number, time: number): number {
     return amount * (1 + (rate / 100) * time);
+  }
+
+  public printWorth(): string {
+    return `After ${this.time} years at ${this.rate}%, the investiment will be worth $${this.worth}.`;
   }
 }
--- ch12-simple-interest/investiment.spec.ts
+++ ch12-simple-interest/investiment.spec.ts
@@ -1,20 +1,40 @@
 import { describe, expect, it } from "bun:test";
 import Investiment from "./Investiment";
 
-describe("Investiment()", () => {
-  it("Should calcute the worth of a investiment", () => {
-    const want = 1758;
+describe("Test class Investiment", () => {
+  const want = 1758;
 
-    const investment = {
-      amount: 1500,
-      rate: 4.3,
-      time: 4,
-    };
+  describe("Test static methods: ", () => {
+    it("Should calcute the worth of a investiment statcly", () => {
+      const investment = {
+        amount: 1500,
+        rate: 4.3,
+        time: 4,
+      };
 
-    const { amount, rate, time } = investment;
+      const { amount, rate, time } = investment;
 
-    const got = Investiment.calculate(amount, rate, time);
+      const got = Investiment.calculate(amount, rate, time);
 
-    expect(got).toBe(want);
+      expect(got).toBe(want);
+    });
+  });
+
+  describe("Test object with state", () => {
+    const inv = new Investiment(1500, 4.3, 4);
+
+    it("Should calculate the worth correctly", () => {
+      const got = inv.calculate();
+
+      expect(got).toBe(want);
+    });
+
+    it("Should print the message of worth correctlty", () => {
+      const got = inv.printWorth();
+      const want =
+        "After 4 years at 4.3%, the investiment will be worth $1758.";
+
+      expect(got).toBe(want);
+    });
   });
 });
